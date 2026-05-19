9b2c12536affa392e2359435fa96024c29386c6b
Alex <alexlev61@proton.me>
2026-05-18 20:02:46 -0700

    Add files via upload

---
+++ midichords/__init__.py
@@ -0,0 +1,1 @@
+# from .midipca import midiano
---
+++ midichords/midichords.py
@@ -0,0 +1,74 @@
+#! /usr/bin/python3
+
+r'''###############################################################################
+###################################################################################
+#
+#
+#	MIDI Chords Python Module
+#	Version 1.0
+#
+#	Project Los Angeles
+#
+#	Tegridy Code 2026
+#
+#   https://github.com/Tegridy-Code/Project-Los-Angeles
+#
+#
+###################################################################################
+###################################################################################
+#
+#   Copyright 2026 Project Los Angeles / Tegridy Code
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+###################################################################################
+'''
+
+###################################################################################
+###################################################################################
+
+print('=' * 70)
+print('Loading midichords Python module...')
+print('Please wait...')
+print('=' * 70)
+
+__version__ = '1.0.0'
+
+print('midichords module version', __version__)
+print('=' * 70)
+
+###################################################################################
+###################################################################################
+
+import MIDI
+import numpy as np
+from scipy import stats
+from collections import Counter, defaultdict
+import math
+import os
+import tqdm
+from sklearn.ensemble import IsolationForest
+from sklearn.preprocessing import StandardScaler
+
+###################################################################################
+
+
+###################################################################################
+
+print('Module is loaded!')
+print('Enjoy! :)')
+print('=' * 70)
+
+###################################################################################
+# This is the end of the MIDI Chords Python module
+###################################################################################
